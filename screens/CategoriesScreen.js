229e5b8ebe306883bf606c5dea3a56f4e54fec4f
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-03-27 11:13:26 -0700

    added template for meal data, added dummy-data to use in development

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -7,13 +7,15 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
   const renderGridItem = itemData => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() => {
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
           props.navigation.navigate({
             routeName: 'CategoryMeals',
             params: {
@@ -21,11 +23,7 @@ const CategoriesScreen = props => {
             },
           });
         }}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+      />
     );
   };
 
