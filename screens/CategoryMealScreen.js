dba2eaa3773804726abd91df2690df18d5670bc1
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-03-27 13:55:16 -0700

    began styling and design of menu items

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { CATEGORIES } from '../data/dummy-data';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = props => {
+  const renderMealItem = itemData => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        image={itemData.item.imageUrl}
+        onSelectMeal={() => {}}
+      />
+    );
+  };
+
   const catId = props.navigation.getParam('categoryId');
-  const selectCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen!</Text>
-      <Text>{selectCategory.title}</Text>
-      <Button
-        title='Go to details'
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: 'MealDetails',
-          });
-        }}
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: '100%' }}
       />
     </View>
   );
